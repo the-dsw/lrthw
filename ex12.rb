2f75d7311fc7b1925b51cea6a893d21cc8ccdc11
the-dsw <fmod@the-dsw.com>
2016-01-08 00:15:32 +0100

    new excercices

---
+++ ex12.rb
@@ -0,0 +1,21 @@
+# print "Give me a number: "
+# number = gets.chomp.to_i
+
+# bigger = number * 100
+# puts "A bigger number is #{bigger}."
+
+# print "Give me another number: "
+# another = gets.chomp
+# number = another.to_i
+
+# smaller = number / 100
+# puts "A smaller number is #{smaller}."
+
+
+print "Give me some money: "
+number = gets.chomp.to_f
+
+mBack = number * 0.1
+money = mBack.to_f
+
+puts "give me back #{money} in dollars"
